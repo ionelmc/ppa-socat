dc620e72c5595e1e5438f304ddd5aadb4c1db4a9
Ionel Cristian Mărieș <contact@ionelmc.ro>
2018-10-12 13:46:20 +0300

    Small fixes. Add bionic.

--- update-ppa.sh
+++ update-ppa.sh
@@ -6,7 +6,7 @@ fi
 if ! which pull-lp-source; then
     sudo apt-get install ubuntu-dev-tools
 fi
-if ! which /dh_autoreconf; then
+if ! which dh_autoreconf; then
     sudo apt-get install dh-autoreconf
 fi
 gpg-agent || eval `gpg-agent --daemon`
@@ -14,17 +14,17 @@ echo ==============================
 echo        making packages
 echo ==============================
 
-for dist in trusty xenial; do
+for dist in trusty xenial bionic; do
     rm -rf socat*
     pull-lp-source socat $dist
     (
-        cd socat*
+        cd socat-*/
         dch --force-distribution -D $dist -l"ionelmc1~ppa" "Enable readline. Disable openssl."
         patch -lp1 < ../enable-readline.patch
         sed -i 's/libssl-dev/libreadline6-dev/' debian/control
         sed -i 's/^Depends: /Depends: libreadline6,/' debian/control
         echo fix-include >> debian/patches/series
-        debuild -S -sd
+        debuild -S -sd --no-check-builddeps
     )
     dput -f ppa:ionel-mc/socat *.changes
 done
